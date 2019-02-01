f5e03af2ec20a6f2b81ae72b47a8b6a247cc79a1
gemplank <info@gpwebdevelopment.co.uk>
2019-02-01 18:13:50 +0000

    Fix author url

--- print-style-css/plugin.php
+++ print-style-css/plugin.php
@@ -8,7 +8,7 @@
  * Description:       A plugin to alter styling when in print view.
  * Version:           0.1.0
  * Author:            Gemma Plank <info@gpwebdevelopment.co.uk>
- * Author URI:        https://gpwebdevlopment.co.uk
+ * Author URI:        https://gpwebdevelopment.co.uk
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  */
